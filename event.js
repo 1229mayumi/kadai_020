7b96f21b828e7f642594888c120909218c369ce2
1229mayumi <letrico1229@yahoo.co.jp>
2023-12-19 22:51:12 +0900

    kadai_020

---
+++ event.js
@@ -0,0 +1,6 @@
+//btnというidを持つHTML要素を取得し、定数に代入する
+const Btn = document.getElementById('btn');
+//HTML要素がクリックされた時にイベント処理を実行する
+Btn.addEventListener('click', () => {
+  console.log('ボタンをクリックしました')
+});
